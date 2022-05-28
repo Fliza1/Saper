02dbc0e159f30ef245c804e92d3f0644235fc7a3
Friki <93681010+Fliza1@users.noreply.github.com>
2022-05-28 15:12:12 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <Windows.h>
 #include <vector>
+#include <ctime>
+#include <conio.h>
+
 using namespace std;
 
 void gotoxy(int x, int y) {
@@ -8,22 +11,41 @@ void gotoxy(int x, int y) {
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), p);
 }
 
+// поле
 class Map {
 private:
-	int size;
-	vector < vector<int>> map;
+	const int BORDER = 100; // граница поля
+	const int EMPTY_CELL = 0; // пустая ячейка 
+	const int MINE = 10; // мина
+	int size; // размер поля включая границы
+	vector <vector<int>> map;
 public:
 	Map() {
 		size = 10;
 	}
+
+	bool isBorder(int x, int y) {
+
+		if (x < 0 || x >= size)
+			return false;
+
+		if (y < 0 || y >= size)
+			return false;
+
+		if (map[x][y] == BORDER) {
+			return true;
+		}
+		return false;
+	}
+
 	void initMap() {
 		for (int i = 0; i < size; i++) {
-			vector<int > temp;
+			vector <int> temp;
 			for (int j = 0; j < size; j++) {
 				if (i == 0 || j == 0 || i == size - 1 || j == size - 1)
-					temp.push_back(100);
+					temp.push_back(BORDER);
 				else
-					temp.push_back(0);
+					temp.push_back(EMPTY_CELL);
 			}
 			map.push_back(temp);
 		}
@@ -31,33 +53,181 @@ public:
 	void show() {
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
-				if(map[i][j] == 100)
+				if(map[j][i] == BORDER)
 					cout << "#";
-				else if (map[i][j] == 0)
+				else if (map[j][i] == EMPTY_CELL)
 					cout << " ";
+				else if (map[j][i] == MINE)
+					cout << "*";
+				else if (map[j][i] >= 1 && map[j][i] <= 8)
+					cout << map[j][i];
 			}
 			cout << endl;
 		}
 	}
+
+	// случайная расстановка мин
+	void SetRandMines(int numMines) {
+
+		// контроль количества мин, которые можно установить на поле
+		if (numMines >= (size - 2) * (size - 2)) {
+			cout << "Too many mines" << endl;
+			return;
+		}
+
+		for (int i = 0; i < numMines; i++) {
+			int x = 0;
+			int y = 0;
+			// поиск пустой ячейки, не занятой миной
+			do {
+				x = rand() % (size - 2) + 1;
+				y = rand() % (size - 2) + 1;
+			} while (map[x][y] == MINE);
+
+			map[x][y] = MINE;
+		}
+	}
+
+	// растановка чисел на игровом поле
+	void SetDigits() {
+		int d = 0;
+		for (int i = 1; i < size - 1; i++) {
+			for (int j = 1; j < size - 1; j++) {
+				if (map[j][i] == MINE)
+					continue;
+				if (map[j][i + 1] == MINE)
+					d++;
+				if (map[j][i - 1] == MINE)
+					d++;
+				if (map[j + 1][i + 1] == MINE)
+					d++;
+				if (map[j + 1][i - 1] == MINE)
+					d++;
+				if (map[j - 1][i + 1] == MINE)
+					d++;
+				if (map[j - 1][i - 1] == MINE)
+					d++;
+				if (map[j + 1][i] == MINE)
+					d++;
+				if (map[j - 1][i] == MINE)
+					d++;
+				map[j][i] = d;
+				d = 0;
+			}
+
+		}
+	}
+};
+
+class Keyboard {
+
+private:
+	int ch = 0;
+
+public:	
+	
+	Keyboard() {
+		ch = 0;
+	}
+
+	void waitKey() {
+		ch = _getch();
+	}
+
+	int getKey() {
+		return ch;
+	}
+};
+
+class Cursor {
+private:	
+
+	int x = 1;
+	int y = 1;
+
+	int tx = 1;
+	int ty = 1;
+
+public:
+
+	void save() {
+		tx = x;
+		ty = y;
+	}
+
+	void undo() {
+		x = tx;
+		y = ty;
+	}
+
+	void incX() {
+		x++;
+	}
+
+	void decX() {
+		x--;
+	}
+	
+	void incY() {
+		y++;
+	}
+
+	void decY() {
+		y--;
+	}
+
+	int getX() {
+		return x;
+	}
+
+	int getY() {
+		return y;
+	}
+
+	void move() {
+		gotoxy(x, y);
+	}
 };
 
 class Game {
-private:
-	void showLogo() {
-		gotoxy(40, 9);
-		cout << "Saper" << endl;
-		Sleep(2000);
-		system("cls");
-	}
 public:
 	void run() {
-		void showLogo();
 		Map map;
 		map.initMap();
+		map.SetRandMines(10);
+		map.SetDigits();
 		map.show();
+
+		Keyboard kb;
+		Cursor cs;
+
+		cs.move();
+
+
+		while (true) {
+			kb.waitKey();
+
+			cs.save();
+
+			switch (kb.getKey()) {
+			case 77: cs.incX(); break; // право
+			case 80: cs.incY(); break; // вниз
+			case 75: cs.decX(); break; // влево
+			case 72: cs.decY(); break; // вверх
+			}
+
+
+			if (map.isBorder(cs.getX(), cs.getY())) {
+				cs.undo();
+			}
+
+			cs.move();
+		}
 	}
 };
 int main() {
+
+	srand(time(0));
 	Game game;
 	game.run();
 }

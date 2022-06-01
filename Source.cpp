256119df4a996246e6ca76592cde709648326113
Friki <93681010+Fliza1@users.noreply.github.com>
2022-06-01 13:29:25 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -11,18 +11,56 @@ void gotoxy(int x, int y) {
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), p);
 }
 
-// поле
+enum ConsoleColor
+{
+	Black = 0,
+	Blue = 1,
+	Green = 2,
+	Cyan = 3,
+	Red = 4,
+	Magenta = 5,
+	Brown = 6,
+	LightGray = 7,
+	DarkGray = 8,
+	LightBlue = 9,
+	LightGreen = 10,
+	LightCyan = 11,
+	LightRed = 12,
+	LightMagenta = 13,
+	Yellow = 14,
+	White = 15
+};
+
+// устанавливает цвет текста и подложки в консоли
+void SetColor(int text, int background)
+{
+	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hStdOut, (WORD)((background << 4) | text));
+}
+
 class Map {
 private:
 	const int BORDER = 100; // граница поля
 	const int EMPTY_CELL = 0; // пустая ячейка 
 	const int MINE = 10; // мина
 	int size; // размер поля включая границы
-	vector <vector<int>> map;
+	vector <vector<int>> map;	
+	vector <vector<int>> mask;
 public:
-	Map() {
+Map() {
 		size = 10;
 	}
+      
+    int openCell(int x, int y) {
+		int result = 1;
+		mask[x][y] = 1;
+		if (map[x][y] == MINE) {
+			result = 0;
+		}
+		show();
+
+		return result;
+}
 
 	bool isBorder(int x, int y) {
 
@@ -38,7 +76,7 @@ public:
 		return false;
 	}
 
-	void initMap() {
+	void initVec(vector <vector<int>> & vec) {
 		for (int i = 0; i < size; i++) {
 			vector <int> temp;
 			for (int j = 0; j < size; j++) {
@@ -47,20 +85,80 @@ public:
 				else
 					temp.push_back(EMPTY_CELL);
 			}
-			map.push_back(temp);
+			vec.push_back(temp);
 		}
 	}
+
+	void initMap() {
+		initVec(map);
+	}
+	
+	void initMask() {
+		initVec(mask);
+	}
+
+	void coutColor(char ch, int color) {
+		SetColor(LightGreen, Black);
+		cout << ch;
+		SetColor(White, Black);
+	}
+
 	void show() {
+		gotoxy(0, 0);
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
-				if(map[j][i] == BORDER)
-					cout << "#";
+
+				if (mask[j][i] == EMPTY_CELL) {
+					cout << ".";
+					continue;
+				}
+				if (map[j][i] == BORDER) {
+					coutColor('#', LightGreen);
+				}
 				else if (map[j][i] == EMPTY_CELL)
 					cout << " ";
 				else if (map[j][i] == MINE)
 					cout << "*";
-				else if (map[j][i] >= 1 && map[j][i] <= 8)
+				else if (map[j][i] == 1) {
+					SetColor(LightCyan, Black);
 					cout << map[j][i];
+					SetColor(White, Black);
+				}
+				else if (map[j][i] == 2) {
+					SetColor(LightMagenta, Black);
+					cout << map[j][i];
+					SetColor(White, Black);
+				}
+				else if (map[j][i] == 3) {
+					SetColor(Yellow, Black);
+					cout << map[j][i];
+					SetColor(White, Black);
+				}
+				else if (map[j][i] == 4) {
+					SetColor(LightBlue, Black);
+					cout << map[j][i];
+					SetColor(White, Black);
+				}
+				else if (map[j][i] == 5) {
+					SetColor(Magenta, Black);
+					cout << map[j][i];
+					SetColor(White, Black);
+				}
+				else if (map[j][i] == 6) {
+					SetColor(Brown, Black);
+					cout << map[j][i];
+					SetColor(White, Black);
+				}
+				else if (map[j][i] == 7) {
+					SetColor(LightRed, Black);
+					cout << map[j][i];
+					SetColor(White, Black);
+				}
+				else if (map[j][i] == 8) {
+					SetColor(Red, Black);
+					cout << map[j][i];
+					SetColor(White, Black);
+				}
 			}
 			cout << endl;
 		}
@@ -191,10 +289,20 @@ public:
 
 class Game {
 public:
+
+	void gameOver() {
+		gotoxy(40, 9);
+		cout << "Game over";
+		Sleep(2000);
+		gotoxy(0, 15);
+		system("pause");
+	}
+
 	void run() {
 		Map map;
 		map.initMap();
-		map.SetRandMines(10);
+		map.initMask();
+		map.SetRandMines(29);
 		map.SetDigits();
 		map.show();
 
@@ -203,8 +311,9 @@ public:
 
 		cs.move();
 
+		bool exit = false;
 
-		while (true) {
+		while (!exit) {
 			kb.waitKey();
 
 			cs.save();
@@ -214,6 +323,13 @@ public:
 			case 80: cs.incY(); break; // вниз
 			case 75: cs.decX(); break; // влево
 			case 72: cs.decY(); break; // вверх
+			case 13:
+				if (map.openCell(cs.getX(), cs.getY()) == 0) {
+					gameOver();
+					exit = true;
+					system("cls");
+				}
+			break; // вверх
 			}
 
 
